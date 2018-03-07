7cecf060d946d74db04fafec9e45989e74680825
Eduardo Perez <ed.perez@icloud.com>
2018-03-07 13:39:45 -0600

    modified the controller api; able to parse some jSON from malk's server and store it in controller for future use

--- controller/controller.php
+++ controller/controller.php
@@ -9,31 +9,10 @@
 // Database initializer(s)
 $host='localhost';
 
-/*
-_  _ ____ ____ ____    ___  ____ ___ ____    ____ ____ _  _ ___ ____ ____ _
-|  | [__  |___ |__/    |  \ |__|  |  |__|    |    |  | |\ |  |  |__/ |  | |
-|__| ___] |___ |  \    |__/ |  |  |  |  |    |___ |__| | \|  |  |  \ |__| |___
-*/
-
-// Mohammad's User dB constants
-$userTable_serverName=$host;
-$userTable_userName='malkhudc_userdb';
-$userTable_password='123456';
-$userTable_dataBaseName='malkhudc_user';
-
-// call connection to mohammed's user database...
-$userDbConnectionInstance=DBConnect($userTable_serverName,$userTable_userName,$userTable_password,$userTable_dataBaseName);
-// query call to select uid that matches
-
-
-
-
-
-
-
+ini_set("allow_url_fopen", 1);
 //@Pre:
 //@Post:
-function DBConnect($serverName,$username,$password,$dbName){
+function DBConnector($serverName,$username,$password,$dbName){
     $db=new mysqli($serverName,$username,$password,$dbName);
     if ($db->connect_errno > 0){
         // print error to web console
@@ -54,8 +33,54 @@ function getHTTPOrderRequestFromSwift(){
 //@Pre: user exists in Database.User
 //@Post: user data is returned from User table
 function getJSONUser(){
-    return true;
-
+    $json = file_get_contents('http://malkhud2.create.stedwards.edu/user/getUser.php');
+    $obj = json_decode($json);
+    echo $obj->access_token;
+    return $obj;
 }
 
-?>
+/*
+_  _ ____ ____ ____    ___  ____ ___ ____    ____ ____ _  _ ___ ____ ____ _
+|  | [__  |___ |__/    |  \ |__|  |  |__|    |    |  | |\ |  |  |__/ |  | |
+|__| ___] |___ |  \    |__/ |  |  |  |  |    |___ |__| | \|  |  |  \ |__| |___
+*/
+
+/*
+ * Parse
+ */
+
+
+
+/*
+// Mohammad's User dB constants
+$userTable_serverName=$host;
+$userTable_userName='malkhudc_userdb';
+$userTable_password='123456';
+$userTable_dataBaseName='malkhudc_user';
+
+// call connection to mohammed's user database...
+$userDbConnectionInstance=DBConnector($userTable_serverName,$userTable_userName,$userTable_password,$userTable_dataBaseName);
+*/
+
+// call to mohammad's getUser.php script
+$user = getJSONUser();
+// grab the ID info from the string
+$user_userID = $user['user_id'];
+$user_password = $user['password'];
+
+/*
+ *  TO DO
+ */
+//$user_currLocationLongitude = $user['']
+//$user_currLocationLatitude = $user['']
+//$user_destLong = $user[''];
+//$user_destLat = $user[''];
+
+
+
+
+
+
+
+
+
