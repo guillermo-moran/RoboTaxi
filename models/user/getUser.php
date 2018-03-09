77413739bad6306ab13c65167209f58e57772ef6
mohammed <malkhud2@stedwards.edu>
2018-03-09 17:09:03 -0600

    update getUser

--- models/user/getUser.php
+++ models/user/getUser.php
@@ -6,9 +6,10 @@
  * Time: 11:39 PM
  */
 
-  $mysqli = new mysqli("localhost", "malkhudc_user", "123456", "malkhudc_userdb");
 
- $result = $mysqli->query("SELECT * FROM user WHERE user_id = 1")->fetch_assoc();
+$mysqli = new mysqli("localhost", "malkhudc_userdb", "123456", "malkhudc_user");
+
+$result = $mysqli->query("SELECT * FROM user WHERE user_id = ". $_GET['user_id'] )-> fetch_assoc();
 
 // $rs = $mysqli->Execute("SELECT * FROM user WHERE user_id = 1" . reset($_GET));
 
@@ -17,29 +18,42 @@
 
 $user = new stdClass();
 
-$user -> user_id = $result['user_id']."<br>";
+$user -> user_id                    = $result['user_id'];
 
-$user -> user_name = $result['user_name']."<br>";
+$user -> user_name                  = $result['user_name'];
 
-$user -> user_firstName = $result['user_firstName']."<br>";
+$user -> user_firstName             = $result['user_firstName'];
 
-$user -> user_lastName = $result['user_lastName']."<br>";
+$user -> user_lastName              = $result['user_lastName'];
 
-$user -> Email = $result['email']."<br>";
+$user -> Email                      = $result['email'];
 
-$user -> password = $result['password']."<br>";
+$user -> password                   = $result['password'];
 
-$user -> credit_card_number = $result['credit_card_number']."<br>";
+$user -> credit_card_number         = $result['credit_card_number'];
 
-$user -> card_holder_name = $result['card_holder_name']."<br>";
+$user -> card_holder_name           = $result['card_holder_name'];
 
-$user -> credit_expiration = $result['credit_expiration']."<br>";
+$user -> credit_expiration          = $result['credit_expiration'];
 
-$user -> credit_ccv = $result['credit_ccv']."<br>";
+$user -> credit_ccv                 = $result['credit_ccv'];
 
 $json = json_encode($user);
-echo $json;
 
 
+if ($user -> user_id == null)
+{
+    http_response_code(404);
+    print "ERROR: user_id not in database!";
+}
+// else http_response_code(202);
+
+else
+{
+    http_response_code(202);
+    print $json;
+}
+
+$mysqli -> close();
 
 
